b865c8d41ac7aad82e8b16438d7e0b0ada7b858e
lesa siroy <lesa@kodego.ph>
2022-11-10 23:26:42 +0800

    Assignments

---
+++ src/main/kotlin/kodego_ph_assignments_siroy/Activity01E.kt
@@ -0,0 +1,35 @@
+//Identify 3 uses cases that will efficiently use an Array, ArrayList, Set, Map.
+
+fun main () {
+
+//Create an application that will accept 2 string inputs.
+// Your application will print all unique characters in both Strings.
+
+    print("Enter your Name:")
+    var name = readln().lowercase()
+
+    println("Name is $name")
+
+
+    print("Enter your Occupation:")
+    var occupation = readln().lowercase()
+
+    println("Occupation is $occupation")
+
+    var setA = mutableSetOf<Char>()
+    var setB = mutableSetOf<Char>()
+
+    for (char in name) {
+        setA.add(char)
+    }
+    println("The unique letters in set A is ${setA.minus((setB))}")
+
+    for (char in occupation) {
+        setB.add(char)
+    }
+    println("The unique letters in set B is ${setB.minus(setA)}")
+
+    println(setA.union(setB))
+    print("Intersecting letters: ")
+    println(setA.intersect(setB))
+}
