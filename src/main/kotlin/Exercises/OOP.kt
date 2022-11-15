6f4dc9177740c970ac463dfdb36bf23517c0a604
lesa siroy <lesa@kodego.ph>
2022-11-15 23:55:05 +0800

    Assignments and Exercises

---
+++ src/main/kotlin/Exercises/OOP.kt
@@ -0,0 +1,20 @@
+/*
+You are tasked to create an application for a fast food company.
+The application will do the following :
+1. Take orders of the customers
+- Create a cart that will contain the items bought by a customer
+- Each cart has a unique id
+- Each card belongs to a customer
+- a customer has an address mobile number  first name and last name.
+2. Show the status of the orders if the are sent to the kitchen, being prepared, for pick up, for delivery, delivered, cancelled, paid, unknown.
+- Use enumaration
+- Create a function "Update order" which will update the cart of a specific customer
+3. Show the items being sold. The fast food company sells fresh fruits, shakes, juices, sandwiches, and salads.
+- Create 5 items for each category of items being sold.
+- Use OOP concepts to create the classes for the food category.
+- You must use Inheritance, Polymorphism, Encapsulation, Abstraction*/
+
+package Exercises
+
+object OOP {
+}
